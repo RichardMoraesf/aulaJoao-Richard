5a2bf232280e15b40236844de70452f3f86d7a0e
RichardMoraesf <richard.moraes-f@hotmail.com>
2022-08-08 22:04:40 -0300

    Aula introdutória sobre JS Aula01

---
+++ PMII/aula01/Aula1.js
@@ -0,0 +1,18 @@
+var a = 3 // declarando variavel com var
+let b = 4 // declarado variavel com let
+console.log('Conteudo de a: ', a,'Conteudo de b: ', b)
+
+var a = 30 // var pode ser redeclarada duas vezes
+b = 40 // let é declarada uma única vez
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5 // declaração de constante, usar pra coisas que não tem 
+// c = 50 // não consegue reatribuir valor
+
+console.log(c)
